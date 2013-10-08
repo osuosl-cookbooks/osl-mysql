afa53b184b6f91d781bd8c9a69f0a4311f14f1bc
Rudy Grigar <rudy@grigar.net>
2013-10-08 13:22:45 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# osl-mysql cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: OSU Open Source Lab (<systems@osuosl.org>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'osl-mysql'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures osl-mysql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
