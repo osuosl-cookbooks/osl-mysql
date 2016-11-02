27a178f8f7f24ff36775be37fd2f2e5977b3082d
fahlmant <fahlmantaylor@gmail.com>
2016-11-02 08:53:58 -0700

    Bump osl-mysql to 1.0.5

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures osl-mysql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+version          '1.0.5'
 
 depends          'database'
 depends          'firewall'
