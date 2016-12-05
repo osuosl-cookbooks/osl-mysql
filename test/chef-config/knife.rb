9e008f180e2d52c23e01d678c54429009ac20165
Lance Albertson <lance@osuosl.org>
2016-12-05 11:42:37 -0800

    Update and cleanup chef provisioning test environment
    The big thing here is moving the .chef folder somewhere else so that the
    cookbook uploader doesn't break when we bump this cookbook.

---
+++ test/chef-config/knife.rb
@@ -0,0 +1,12 @@
+current_dir = File.dirname(__FILE__)
+node_name 'fakeclient'
+client_key "#{current_dir}/fakeclient.pem"
+validation_client_name 'chef-validator'
+validation_key "#{current_dir}/validator.pem"
+chef_server_url 'https://api.opscode.com/organizations/my_awesome_org'
+cache_type 'BasicFile'
+cache_options(path: "#{ENV['HOME']}/.chef/checksums")
+cookbook_path ["#{current_dir}/../../cookbooks"]
+role_path "#{current_dir}/../integration/roles"
+data_bag_path "#{current_dir}/../integration/data_bags"
+knife[:secret_file] = "#{current_dir}../integration/encrypted_data_bag_secret"
