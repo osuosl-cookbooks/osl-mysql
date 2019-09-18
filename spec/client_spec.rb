e8fad7f1cf484e09b8cd42ccf79bb2eafc01f42c
Lance Albertson <lance@osuosl.org>
2019-09-18 14:26:36 -0700

    Update ChefSpec for percona client

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -12,14 +12,37 @@ describe 'osl-mysql::client' do
       it do
         expect(chef_run).to include_recipe('osl-mysql')
       end
+      it do
+        expect(chef_run).to_not include_recipe('percona::client')
+      end
       case pltfrm
       when CENTOS_7_OPTS
         it do
-          expect(chef_run).to install_package('mariadb')
+          expect(chef_run).to create_mysql_client('default').with(package_name: %w(mariadb mariadb-devel))
         end
       when CENTOS_6_OPTS
         it do
-          expect(chef_run).to create_mysql_client('default')
+          expect(chef_run).to create_mysql_client('default').with(package_name: %w(mysql mysql-devel))
+        end
+      end
+      context 'percona client' do
+        cached(:chef_run) do
+          ChefSpec::SoloRunner.new(pltfrm) do |node|
+            node.normal['osl-mysql']['enable_percona_client'] = true
+          end.converge(described_recipe)
+        end
+        it do
+          expect(chef_run).to include_recipe('percona::client')
+        end
+        case pltfrm
+        when CENTOS_7_OPTS
+          it do
+            expect(chef_run).to_not create_mysql_client('default').with(package_name: %w(mariadb mariadb-devel))
+          end
+        when CENTOS_6_OPTS
+          it do
+            expect(chef_run).to_not create_mysql_client('default').with(package_name: %w(mysql mysql-devel))
+          end
         end
       end
     end
