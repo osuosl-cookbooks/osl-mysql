ac88f3fb31d7afe0770b1a7edb763a45c7b443c3
Robert Detjens <detjensrobert@osuosl.org>
2019-12-18 18:30:28 -0800

    bump chef_version to >= 14.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ source_url       'https://github.com/osuosl-cookbooks/osl-mysql'
 maintainer       'Oregon State University'
 maintainer_email 'systems@osuosl.org'
 license          'Apache-2.0'
-chef_version     '>=12.18' if respond_to?(:chef_version)
+chef_version     '>= 14.0'
 description      'Installs/Configures osl-mysql'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.2.3'
