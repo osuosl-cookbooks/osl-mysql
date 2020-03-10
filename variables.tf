8f84682f4cf53aa9d1cb58c9f252485e119e35b0
Andrew Dassonville <andrewda@osuosl.org>
2020-03-10 13:17:42 -0700

    Update terraform variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "chef_version" {
-    default = "14.14.29"
+    default = "14"
 }
 variable "centos_atomic_image" {
     default = "CentOS Atomic 7.1902"
 }
 variable "centos_image" {
-    default = "CentOS 7.6"
+    default = "CentOS 7.7"
 }
 variable "ssh_key_name" {
     default = "bootstrap"
