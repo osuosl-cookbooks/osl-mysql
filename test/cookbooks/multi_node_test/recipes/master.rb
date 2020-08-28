383f6c0b6611377336e56459eda5d9f81bfeb8ff
Ethan Hampton <hamptone@osuosl.org>
2020-08-28 14:47:03 -0700

    Mid-work centos 8 changes
    Still need to get SSL between Centos 8 nodes working,
    figure out packages for mon and clean up the rest of this the files

--- test/cookbooks/multi_node_test/recipes/master.rb
+++ test/cookbooks/multi_node_test/recipes/master.rb
@@ -1,39 +1,30 @@
+# node.default['percona']['server']['replication']['ssl_enabled'] = true
+
 include_recipe 'multi_node_test::network'
 include_recipe 'osl-mysql::master'
 
-mysql_connection_info = {
-  host: '127.0.0.1',
-  username: 'root',
-  password: 'jzYY0cQUnPAMcqvIxYaC',
-}
+mysql_password = 'jzYY0cQUnPAMcqvIxYaC'
 
-build_essential 'mysql'
-package 'ruby-devel'
-
-mysql2_chef_gem 'default' do
-  provider Chef::Provider::Mysql2ChefGem::Percona
-  action :install
-end
-
-mysql_database 'testdb' do
-  connection mysql_connection_info
-  action :create
-end
-
-mysql_database 'testdb' do
-  connection mysql_connection_info
-  sql 'DROP TABLE IF EXISTS example'
+mariadb_database 'testdb' do
+  password mysql_password
+  sql 'CREATE DATABASE testdb;'
   action :query
 end
 
-mysql_database 'testdb' do
-  connection mysql_connection_info
-  sql 'CREATE TABLE IF NOT EXISTS example (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(32))'
+mariadb_database 'testdb' do
+  password mysql_password
+  sql 'USE testdb; DROP TABLE IF EXISTS example'
   action :query
 end
 
-mysql_database 'testdb' do
-  connection mysql_connection_info
-  sql 'INSERT INTO example (name) VALUES (\'hello\'), (\'world\')'
+mariadb_database 'testdb' do
+  password mysql_password
+  sql 'USE testdb; CREATE TABLE IF NOT EXISTS example (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(32))'
+  action :query
+end
+
+mariadb_database 'testdb' do
+  password mysql_password
+  sql 'USE testdb; INSERT INTO example (name) VALUES (\'hello\'), (\'world\')'
   action :query
 end
--- test/cookbooks/multi_node_test/recipes/slave.rb
+++ test/cookbooks/multi_node_test/recipes/slave.rb
@@ -1,2 +1,4 @@
+#node.default['percona']['server']['replication']['ssl_enabled'] = true
+
 include_recipe 'multi_node_test::network'
 include_recipe 'osl-mysql::slave'
