70d0e8facba283b837e6f00715a442f01dad7177
Ethan Hampton <hamptone@osuosl.org>
2020-09-02 12:07:00 -0700

    Fix SSL keys for multi-node test

--- test/cookbooks/multi_node_test/recipes/master.rb
+++ test/cookbooks/multi_node_test/recipes/master.rb
@@ -1,4 +1,4 @@
-# node.default['percona']['server']['replication']['ssl_enabled'] = true
+node.default['percona']['server']['replication']['ssl_enabled'] = true if node['platform_version'].to_i >= 8
 
 include_recipe 'multi_node_test::network'
 include_recipe 'osl-mysql::master'
