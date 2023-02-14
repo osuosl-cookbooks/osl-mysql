c7ea004058550ed3f46ac129f1f3773ba4be6736
Zachary Wallace <zwall@osuosl.org>
2023-02-13 16:45:32 -0800

    Added custom root passwd and dynamic database keyvals

--- resources/osl_mysql_test.rb
+++ resources/osl_mysql_test.rb
@@ -1,5 +1,5 @@
-provides :osl_mysql_dev
-resource_name :osl_mysql_dev
+provides :osl_mysql_test
+resource_name :osl_mysql_test
 unified_mode true
 
 default_action :create
@@ -7,23 +7,30 @@ default_action :create
 property :database, String, name_property: true
 property :username, String, required: true
 property :password, String, required: true
+property :server_password, String, default: lazy { 'osl_mysql_test' }
+property :encoding, String, default: lazy { 'utf8mb4' }
+property :collation, String, default: lazy { 'utf8mb4_unicode_ci' }
+property :database_parameters, Hash, default: {}
 
 # Install the mariadb package, set up the service, set up the user, then set up the given database
 action :create do
   # Install the package, and set up the service
   mariadb_server_install 'MariaDB' do
-    password 'iloveinsecurepasswords'
+    password new_resource.server_password
     action [:install, :create]
   end
   # Create new database
   mariadb_database new_resource.database do
-    password 'iloveinsecurepasswords'
-    encoding 'utf8mb4'
-    collation 'utf8mb4_unicode_ci'
+    password new_resource.server_password
+    encoding new_resource.encoding
+    collation new_resource.collation
+    new_resource.database_parameters.each do |key, value|
+      send(key.to_sym, value)
+    end
   end
   # Grant privilages
   mariadb_user new_resource.username do
-    ctrl_password 'iloveinsecurepasswords'
+    ctrl_password new_resource.server_password
     password new_resource.password
     database_name new_resource.database
     action [:create, :grant]
--- test/cookbooks/resources_test/recipes/osl_mysql_test.rb
+++ test/cookbooks/resources_test/recipes/osl_mysql_test.rb
@@ -1,11 +1,11 @@
 # Initialize the database service, and create a database
-osl_mysql_dev 'foobar' do
+osl_mysql_test 'foobar' do
   username 'foo'
   password 'foofoo'
 end
 
 # Add in an extra database
-osl_mysql_dev 'barfoo' do
+osl_mysql_test 'barfoo' do
   username 'foo'
   password 'foofoo'
 end
